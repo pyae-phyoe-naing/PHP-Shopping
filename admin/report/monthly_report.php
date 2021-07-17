21dd89135a61a2a08ed9c2e587056fd8a7c7d510
fazza7777 <aungbarlaay7777@gmail.com>
2021-07-17 12:40:57 +0630

    mnothly report

--- admin/report/monthly_report.php
+++ admin/report/monthly_report.php
@@ -2,7 +2,7 @@
 require('../../init.php');
 $title = 'Monthly Report';
 if (!isset($_SESSION['user'])) {
-back('errorModal', 'Account Login ဝင်ရန်လိုအပ်ပါသည်။','../login.php');
+    back('errorModal', 'Account Login ဝင်ရန်လိုအပ်ပါသည်။', '../login.php');
 }
 
 
@@ -25,13 +25,50 @@ require('../layout/header.php');
 
         </div>
     </div>
+    <?php
+    $now = date('Y-m-d');
+    //  $from = date('Y-m-d', strtotime('-7 days', strtotime($now))); OR
+    $from = date('Y-m-d', strtotime($now . ' -1 months'));
+    $data = getAll("select *,(select name from users where sale_orders.user_id=users.id) as user
+                   from sale_orders where cast(order_date as date)<='$now' and cast(order_date as date)>'$from' ");
+    // pretty($data);
+   
+    ?>
     <!-- Main Content -->
     <div class="row">
         <div class="col-12">
             <div class="card">
-                <div class="card-header"> List</div>
+                <div class="card-header">Monthly Report List</div>
                 <div class="card-body">
-                   
+                    <table class="table" id="weekly_report" class="display" style="width:100%">
+                        <thead>
+                            <tr>
+                                <th>No</th>
+                                <th>Customer</th>
+                                <th>Total Amount</th>
+                                <th>Order Date</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php
+                            $i = 1;
+                            foreach ($data as $od) {
+                            ?>
+                                <tr>
+                                    <td><?php echo $i++; ?></td>
+                                    <td><?php echo $od['user'] ?></td>
+                                    <td><?php echo $od['total_price']; ?> MMK</td>
+                                    <td>
+                                        <span class="mr-3">
+                                            <i class="pe-7s-date mr-1"></i>
+                                            <?php echo date_format(date_create($od['order_date']), "d F Y "); ?>
+                                        </span>
+
+                                    </td>
+                                </tr>
+                            <?php } ?>
+                        </tbody>
+                    </table>
                 </div>
             </div>
         </div>
@@ -41,29 +78,10 @@ require('../layout/header.php');
 
 <?php require('../layout/footer.php') ?>
 <script>
-    function deleteCat(slug) {
-
-        Swal.fire({
-            title: 'Are you sure delete?',
-            text: "You won't be able to revert this!",
-            icon: 'warning',
-            reverseButtons: true,
-            confirmButtonText: 'Confirm',
-            confirmButtonColor: '#3085d6',
-            cancelButtonColor: '#d33',
-            showCancelButton: true,
-            preConfirm: () => {
-                $.get(`delete.php?slug=${slug}`)
-            },
-        }).then(res => {
-            if(res.isConfirmed){
-                location.reload();
-            }
-        })
-
-    }
+    $(document).ready(function() {
+        $('#weekly_report').DataTable();
+    });
 </script>
-
 </body>
 
 </html>
